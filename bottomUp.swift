92e3c23dfb7eaff5210e0ce1ae8d3d39f684b785
Connor <connor@wybro.com>
2017-12-09 23:12:50 -0500

    Code cleanup

--- bottomUp.swift
+++ bottomUp.swift
@@ -6,43 +6,28 @@ public struct BottomUp {
     var io = 0
 
     var mutGraph = graph
-    // print("Graph before sorting: \(mutGraph)")
 
-    // ascending order of their degree
-    // mutGraph.sorted()
-
-
-    // Vertices in ascending order by degree
     var vertices = mutGraph.vertices.sorted { left, right in
       graph.vertexDegree(left) < graph.vertexDegree(right)
     }
-    // print("VERTICES: \(vertices)")
 
     var results = [[Int]](repeating: [], count: mutGraph.maxDegree()+1)
 
     while !mutGraph.empty {
-      // print("-- NOT EMPTY: \(mutGraph)")
       io += 1
       let d = mutGraph.minDegree()
-      // print("MIN: \(d)")
+
       while mutGraph.vertexWith(degree: d) {
-        // print("---- VERTEX WITH DEGREE \(d): \(mutGraph)")
-        // let v = mutGraph.nextVertex
         let v = vertices.removeFirst()
-        // print("---- VERTICES: \(vertices)")
+
         results[d].append(v)
         mutGraph.delete(vertex: v)
-        // mutGraph.sorted()
+
         vertices = vertices.sorted { left, right in
           graph.vertexDegree(left) < graph.vertexDegree(right)
         }
-        // print("---- VERTICES: \(vertices)")
-        // print("---- AFTER CHANGES: \(mutGraph)")
       }
     }
-
-    // print("Min Degree: \(mutGraph.minDegree())")
-    // print("Max Degree: \(mutGraph.maxDegree())")
     print("- BottomUp -- I/O's: \(io)\n")
     return results
   }
